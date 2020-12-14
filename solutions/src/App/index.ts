2727677b61110b24771f110ece536b4d86ecde8a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-12-14 09:29:32 +0000

    step 2

--- solutions/src/App/index.ts
+++ solutions/src/App/index.ts
@@ -1,9 +1,20 @@
-import * as R from "@app/Reader"
-import * as T from "@app/Task"
-import * as E from "@app/Either"
+import * as R from "@app/Reader";
+import * as T from "@app/Task";
+import * as E from "@app/Either";
 
-export type App<R, E, A> = R.Reader<R, T.Task<E.Either<E, A>>>
+export type App<R, E, A> = R.Reader<R, T.Task<E.Either<E, A>>>;
 
 export function succeed<A>(a: A): App<unknown, never, A> {
-    return () => () => Promise.resolve(E.right(a))
+  return () => () => Promise.resolve(E.right(a));
+}
+
+export function fail<E>(e: E): App<unknown, E, never> {
+  return () => () => Promise.resolve(E.left(e));
+}
+
+export function sync<A>(f: () => A): App<unknown, never, A> {
+  return () => () =>
+    new Promise((res) => {
+      res(E.right(f()));
+    });
 }
--- solutions/test/app.test.ts
+++ solutions/test/app.test.ts
@@ -1,13 +1,34 @@
-import * as App from "@app/App"
-import * as E from "@app/Either"
+import * as App from "@app/App";
+import * as E from "@app/Either";
 
 describe("App", () => {
-    it("should use succeed", async () => {
-        const program = App.succeed(0)
-        const main = program({})
+  it("should use succeed", async () => {
+    const program = App.succeed(0);
 
-        const result = await main()
+    const main = program({});
 
-        expect(result).toEqual(E.right(0))
-    })
-})
+    const result = await main();
+
+    expect(result).toEqual(E.right(0));
+  });
+  it("should use fail", async () => {
+    const program = App.fail(0);
+
+    const main = program({});
+
+    const result = await main();
+
+    expect(result).toEqual(E.left(0));
+  });
+  it("should use sync", async () => {
+    const program = App.sync(() => {
+      return 1 + 1;
+    });
+
+    const main = program({});
+
+    const result = await main();
+
+    expect(result).toEqual(E.right(2));
+  });
+});
